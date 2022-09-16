4b4958b2b0258fac41def63b17dfc714de4b002c
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-09-16 18:37:11 +0530

    data classes for responses created, Api class added, gradle implementations added

---
+++ app/src/main/java/com/example/sptapi/TypicodeApi.kt
@@ -0,0 +1,35 @@
+package com.example.sptapi
+
+import com.example.sptapi.api_resource.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+val retrofit = Retrofit
+    .Builder()
+    .baseUrl("https://jsonplaceholder.typicode.com")
+    .addConverterFactory(GsonConverterFactory.create())
+
+interface TypicodeApi {
+    @GET("/posts")
+    suspend fun getPosts(): List<Posts>
+
+    @GET("/comments")
+    suspend fun getComments(): List<Comments>
+
+    @GET("/albums")
+    suspend fun getAlbums(): List<Albums>
+
+    @GET("/photos")
+    suspend fun getPhotos(): List<Photos>
+
+    @GET("/todos")
+    suspend fun getTodos(): List<Todos>
+
+    @GET("/users")
+    suspend fun getUsers(): List<Users>
+}
+
+enum class CallStatus {
+    LOADING, ERROR, DONE
+}
---
+++ app/src/main/java/com/example/sptapi/api_resource/Albums.kt
@@ -0,0 +1,7 @@
+package com.example.sptapi.api_resource
+
+data class Albums(
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
