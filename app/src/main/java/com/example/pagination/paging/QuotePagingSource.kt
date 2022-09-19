47084e309a82823eae49f97fef56c10041d34f8a
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-09-19 16:18:22 +0530

    paging class added

---
+++ app/src/main/java/com/example/pagination/paging/QuotePagingSource.kt
@@ -0,0 +1,29 @@
+package com.example.pagination.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.pagination.model.Result
+import com.example.pagination.retro.QuotesApi
+
+class QuotePagingSource(private val quotesApi: QuotesApi) : PagingSource<Int, Result>() {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Result> {
+        return try {
+            val position = params.key ?: 1
+            val response = quotesApi.getQuotes(position)
+            LoadResult.Page(
+                data = response.results,
+                prevKey = if (position == 1) null else position - 1,
+                nextKey = if (position == response.totalPages) null else position + 1
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Result>): Int? {
+        return state.anchorPosition?.let {
+            state.closestPageToPosition(it)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(it)?.nextKey?.minus(1)
+        }
+    }
+}
